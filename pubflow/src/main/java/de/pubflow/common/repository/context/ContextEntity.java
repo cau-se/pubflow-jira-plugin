8923334962669b706726439722b343da72524666
Peer Brauer <peer.brauer@informatik.uni-kiel.de>
2013-07-17 16:28:50 +0200

    Added repos

---
+++ pubflow/src/main/java/de/pubflow/common/repository/context/ContextEntity.java
@@ -0,0 +1,29 @@
+package de.pubflow.common.repository.context;
+
+import java.io.Serializable;
+import java.util.Map;
+
+public class ContextEntity implements Serializable{
+
+	private static final long serialVersionUID = 2746891162274753965L;
+	
+	private String serviceUrl;
+	private Map<String, String> parameters;
+
+	public String getServiceUrl() {
+		return serviceUrl;
+	}
+
+	public void setServiceUrl(String serviceUrl) {
+		this.serviceUrl = serviceUrl;
+	}
+
+	public Map<String, String> getParameters() {
+		return parameters;
+	}
+
+	public void setParameters(Map<String, String> parameters) {
+		this.parameters = parameters;
+	}
+	
+}
---
+++ pubflow/src/main/java/de/pubflow/common/repository/context/ContextProvider.java
@@ -0,0 +1,25 @@
+package de.pubflow.common.repository.context;
+
+import de.pubflow.common.repository.abstractRepository.interaction.BasicProvider;
+import de.pubflow.common.repository.abstractRepository.repository.ERepositoryName;
+import de.pubflow.common.repository.abstractRepository.storageAdapter.DBStorageAdapter;
+
+
+
+public class ContextProvider extends BasicProvider<ContextEntity>{
+
+	private static ContextProvider cp;
+
+	public ContextProvider() {
+		super(ERepositoryName.CONTEXT, new DBStorageAdapter());
+	}
+
+	public static ContextProvider getInstance(){
+		if(cp == null){
+			cp = new ContextProvider();
+		}
+		
+		return cp;
+	}
+	
+}
