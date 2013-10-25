e39ae7f26d516c121895aa93af3f0b1e5578f598
Arnd Lange <arnd.lange@informatik.uni-kiel.de>
2013-10-25 10:05:38 +0200

    uncommented ws access methods

--- pubflow/src/main/java/de/pubflow/components/jiraConnector/JiraPluginMsgConsumer.java
+++ pubflow/src/main/java/de/pubflow/components/jiraConnector/JiraPluginMsgConsumer.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
@@ -41,7 +42,6 @@ public class JiraPluginMsgConsumer {
 				Service service = JiraEndpointService.create(new URL("http://localhost:8889/JiraEndpoint?wsdl"), new QName("http://webservice.jira.pubflow.de/", "JiraEndpointService"));
 				jiraEndpoint = service.getPort(IJiraEndpoint.class);
 			} catch (MalformedURLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -49,50 +49,50 @@ public class JiraPluginMsgConsumer {
 		return jiraEndpoint;
 	}
 
-	//	private void addIssueComment(HashMap<String, String> map){
-	//		getJiraEndpoint().addIssueComment(map.get("issueKey"), map.get("comment"));
-	//	}
-	//
-	//	private void changeStatus(HashMap<String, String> map){
-	//		getJiraEndpoint().changeStatus(map.get("issueKey"), map.get("statusId"));
-	//	}
-	//
+	private void addIssueComment(HashMap<String, String> map){
+		getJiraEndpoint().addIssueComment(map.get("issueKey"), map.get("comment"));
+	}
+
+	private void changeStatus(HashMap<String, String> map){
+		getJiraEndpoint().changeStatus(map.get("issueKey"), map.get("statusId"));
+	}
+
 	private void addAttachment(HashMap<String, String> map){
 		byte[] data = Charset.forName(StandardCharsets.UTF_8.name()).encode(map.get("attachmentString")).array();
 
 		System.out.println("received!");
 		System.out.println(getJiraEndpoint().addAttachment(map.get("issueKey"), data,  map.get("attachmentFileName"), map.get("attachmentFileType")));
 	}
-	//
-	//	private void createIssueType(HashMap<String, String> map){
-	//		de.pubflow.components.jiraConnector.wsArtifacts.CreateIssueType.Parameters params = 
-	//				new de.pubflow.components.jiraConnector.wsArtifacts.CreateIssueType.Parameters();
-	//
-	//		
-	//		for(Entry<String, String> entry : CamelJiraMessage.getMap("parameters", map).entrySet()){
-	//			de.pubflow.components.jiraConnector.wsArtifacts.CreateIssueType.Parameters.Entry newEntry = 
-	//					new de.pubflow.components.jiraConnector.wsArtifacts.CreateIssueType.Parameters.Entry();
-	//			
-	//			newEntry.setKey(entry.getKey());
-	//			newEntry.setValue(entry.getValue());
-	//			params.getEntry().add(newEntry);
-	//		}
-	//
-	//		getJiraEndpoint().createIssueType("PubFlow", map.get("wfName"), params);
-	//	}
-	//
-	//	private void createIssue(HashMap<String, String> map){
-	//		de.pubflow.components.jiraConnector.wsArtifacts.CreateIssue.Parameters params = 
-	//				new de.pubflow.components.jiraConnector.wsArtifacts.CreateIssue.Parameters();
-	//		for(Entry<String, String> entry : CamelJiraMessage.getMap("parameters", map).entrySet()){
-	//			de.pubflow.components.jiraConnector.wsArtifacts.CreateIssue.Parameters.Entry newEntry = 
-	//					new de.pubflow.components.jiraConnector.wsArtifacts.CreateIssue.Parameters.Entry();
-	//			newEntry.setKey(entry.getKey());
-	//			newEntry.setValue(entry.getValue());
-	//			params.getEntry().add(newEntry);
-	//		}
-	//		getJiraEndpoint().createIssue("PUB", map.get("wfName"), map.get("summary"), map.get("description"), params, "admin");
-	//	}
+
+	private void createIssueType(HashMap<String, String> map){
+		de.pubflow.components.jiraConnector.wsArtifacts.CreateIssueType.Parameters params = 
+				new de.pubflow.components.jiraConnector.wsArtifacts.CreateIssueType.Parameters();
+
+
+		for(Entry<String, String> entry : CamelJiraMessage.getMap("parameters", map).entrySet()){
+			de.pubflow.components.jiraConnector.wsArtifacts.CreateIssueType.Parameters.Entry newEntry = 
+					new de.pubflow.components.jiraConnector.wsArtifacts.CreateIssueType.Parameters.Entry();
+
+			newEntry.setKey(entry.getKey());
+			newEntry.setValue(entry.getValue());
+			params.getEntry().add(newEntry);
+		}
+
+		getJiraEndpoint().createIssueType("PubFlow", map.get("wfName"), params);
+	}
+
+	private void createIssue(HashMap<String, String> map){
+		de.pubflow.components.jiraConnector.wsArtifacts.CreateIssue.Parameters params = 
+				new de.pubflow.components.jiraConnector.wsArtifacts.CreateIssue.Parameters();
+		for(Entry<String, String> entry : CamelJiraMessage.getMap("parameters", map).entrySet()){
+			de.pubflow.components.jiraConnector.wsArtifacts.CreateIssue.Parameters.Entry newEntry = 
+					new de.pubflow.components.jiraConnector.wsArtifacts.CreateIssue.Parameters.Entry();
+			newEntry.setKey(entry.getKey());
+			newEntry.setValue(entry.getValue());
+			params.getEntry().add(newEntry);
+		}
+		getJiraEndpoint().createIssue("PUB", map.get("wfName"), map.get("summary"), map.get("description"), params, "admin");
+	}
 
 	@Consume(uri="t2-jms:jira:toJira.queue")
 	public void onMsg(String env){
@@ -100,10 +100,10 @@ public class JiraPluginMsgConsumer {
 		CamelJiraMessage msg = MessageToolbox.loadFromString(env, CamelJiraMessage.class);
 
 		switch(msg.getAction()){
-		//		case "jira.newComment":  addIssueComment(msg.getMessage()) ;break;
-		//		case "jira.newIssue": createIssue(msg.getMessage()) ;break;
-		//		case "jira.newIssueType": createIssueType(msg.getMessage()) ;break;
-		//		case "jira.changeStatus": changeStatus(msg.getMessage()) ;break;
+		case "jira.newComment":  addIssueComment(msg.getMessage()) ;break;
+		case "jira.newIssue": createIssue(msg.getMessage()) ;break;
+		case "jira.newIssueType": createIssueType(msg.getMessage()) ;break;
+		case "jira.changeStatus": changeStatus(msg.getMessage()) ;break;
 		case "jira.addAttachment": addAttachment(msg.getMessage()) ;break;
 		}
 	}
