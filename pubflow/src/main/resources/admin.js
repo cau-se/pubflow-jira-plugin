59d57cff158ce40ba25c12365bf49be1dfc2a659
Arnd Plumhoff <arl@informatik.uni-kiel.de>
2014-11-25 13:27:52 +0100

    admin view

---
+++ pubflow/src/main/resources/admin.js
@@ -0,0 +1,29 @@
+(function ($) { 
+    var url = AJS.contextPath() + "/rest/pubflow-admin/1.0/";
+
+    $(document).ready(function() {
+        $.ajax({
+            url: url,
+            dataType: "json"
+        }).done(function(config) { 
+            $("#name").val(config.name);
+            $("#time").val(config.time);
+        });
+    });
+
+})(AJS.$ || jQuery);
+
+function updateConfig() {
+  AJS.$.ajax({
+    url: baseUrl + "/rest/pubflow-admin/1.0/",
+    type: "PUT",
+    contentType: "application/json",
+    data: '{ "name": "' + AJS.$("#name").attr("value") + '", "time": ' +  AJS.$("#time").attr("value") + ' }',
+    processData: false
+  });
+}
+
+AJS.$("#admin").submit(function(e) {
+        e.preventDefault();
+        updateConfig();
+    });
