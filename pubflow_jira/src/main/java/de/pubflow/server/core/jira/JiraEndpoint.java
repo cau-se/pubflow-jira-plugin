a5601a58813069847e8a36cc9dc6c438dc36fec1
Alexander Barbie <abar@informatik.uni-kiel.de>
2016-08-08 11:23:44 +0200

    sachen auskommentiert

--- pubflow_jira/src/main/java/de/pubflow/server/core/jira/JiraEndpoint.java
+++ pubflow_jira/src/main/java/de/pubflow/server/core/jira/JiraEndpoint.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2016 Marc Adolf, Arnd Plumhoff (http://www.pubflow.uni-kiel.de/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.pubflow.server.core.jira;
 
 import java.util.HashMap;
@@ -35,7 +50,8 @@ public class JiraEndpoint{
 
 	public static String createIssue(String issueTypeName, String summary, String description, HashMap<String, String> parameters, String reporter) {
 		try {
-			return JiraObjectCreator.createIssue("PUB", issueTypeName, summary, description, reporter, JiraManagerPlugin.user, parameters);
+//			return JiraObjectCreator.createIssue("PUB", issueTypeName, summary, description, reporter, JiraManagerPlugin.user, parameters);
+			return "moin";
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -65,7 +81,8 @@ public class JiraEndpoint{
 	
 	public static String createIssue(JiraIssue issue) {
 		try {
-			return JiraObjectCreator.createIssue("PUB", issue.getIssueTypeName(), issue.getSummary(), issue.getDescription(), issue.getReporter(), JiraManagerPlugin.user, issue.getParameters());
+//			return JiraObjectCreator.createIssue("PUB", issue.getIssueTypeName(), issue.getSummary(), issue.getDescription(), issue.getReporter(), JiraManagerPlugin.user, issue.getParameters());
+			return "moin";
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -155,7 +172,8 @@ public class JiraEndpoint{
 
 	public static boolean createProject(String projectName, String projectKey, String workflowXML, LinkedList<String> steps) {
 		try {
-			JiraObjectCreator.createProject(projectName, projectKey, JiraManagerPlugin.user, false);
+//			JiraObjectCreator.createProject(projectName, projectKey, JiraManagerPlugin.user, false);
+			return true;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
