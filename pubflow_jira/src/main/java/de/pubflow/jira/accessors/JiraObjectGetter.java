53f2c37c9e21d3d0fa04dd39387543f8f810e33c
Alexander Barbie <abar@informatik.uni-kiel.de>
2016-08-08 13:41:50 +0200

    trying to fix the nullpointer exception by changing the issuetype search function

--- pubflow_jira/src/main/java/de/pubflow/jira/accessors/JiraObjectGetter.java
+++ pubflow_jira/src/main/java/de/pubflow/jira/accessors/JiraObjectGetter.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2016 Marc Adolf, Arnd Plumhoff (http://www.pubflow.uni-kiel.de/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.pubflow.jira.accessors;
 
 import java.util.ArrayList;
@@ -48,17 +63,17 @@ public class JiraObjectGetter {
 	 * @return the issueType we looked for (null if it does not exist)
 	 */
 	public static IssueType getIssueTypeByName(String issueTypeName) {
-		IssueType issueType = null;
-
-		Collection<IssueType> issueTypes = ComponentAccessor.getConstantsManager().getAllIssueTypeObjects();
-
-		for(IssueType tempIssueType : issueTypes) {
-			if(tempIssueType.getName().equals(issueTypeName)) {
-				return tempIssueType;
-			}
-		}
-
-		return issueType;
+	    IssueType issueType = null;
+	    
+	    Collection<IssueType> issueTypes = ComponentAccessor.getConstantsManager().getAllIssueTypeObjects();
+	    
+	    for(IssueType tempIssueType : issueTypes) {
+	      if(tempIssueType.getName().equals(issueTypeName)) {
+	        return tempIssueType;
+	      }
+	    }
+	    
+	    return issueType;
 	}
 
 	/**
