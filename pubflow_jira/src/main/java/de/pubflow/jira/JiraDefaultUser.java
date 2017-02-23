adac2da1c1f5dfabe76bf817b612799b6797524b
Alexander Barbie <abar@informatik.uni-kiel.de>
2017-02-23 15:57:47 +0100

    bugfix, changed usergroup from „librarians“ to „librarian“

--- pubflow_jira/src/main/java/de/pubflow/jira/JiraDefaultUser.java
+++ pubflow_jira/src/main/java/de/pubflow/jira/JiraDefaultUser.java
@@ -74,7 +74,7 @@ public class JiraDefaultUser {
 			InvalidGroupException, GroupNotFoundException, UserNotFoundException, OperationFailedException {
 		groupDataManager = JiraObjectCreator.createGroup("datamanagers");
 		groupScientists = JiraObjectCreator.createGroup("scientists");
-		groupLibrarian = JiraObjectCreator.createGroup("librarians");
+		groupLibrarian = JiraObjectCreator.createGroup("librarian");
 		groupLibrariansFn = JiraObjectCreator.createGroup("librarian-notification");
 		groupDataManagersFn = JiraObjectCreator.createGroup("scientists-notification");
 
